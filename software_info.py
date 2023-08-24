95c7b73cd19b15e96e07062bbf7e98002cada624
unknown <nhanhoa21012002@gmail.com>
2023-08-24 09:34:00 +0700

    cross platform

--- software_info.py
+++ software_info.py
@@ -1,13 +1,13 @@
+import platform
 import subprocess
 
-
-def get_installed_software():
+def get_installed_software_linux():
     try:
         dpkg_output = subprocess.check_output(["dpkg", "--list"], text=True)
-        dpkg_lines = dpkg_output.strip().split("\n")
+        dpkg_lines = dpkg_output.strip().split("\n")[5:]  # Skip header lines
         software_list = []
 
-        for line in dpkg_lines[5:]:  # Skip header lines
+        for line in dpkg_lines:
             columns = line.split()
             if len(columns) >= 3:
                 package_info = {
@@ -20,11 +20,30 @@ def get_installed_software():
     except subprocess.CalledProcessError:
         return []
 
+def get_installed_software_windows():
+    try:
+        powershell_command = "Get-WmiObject -Class Win32_Product | Select-Object Name, Version"
+        powershell_output = subprocess.check_output(["powershell", "-Command", powershell_command], text=True)
+        powershell_lines = powershell_output.strip().split("\n")
+        software_list = []
 
-# software_list = get_installed_software()
+        for line in powershell_lines[2:]:  # Skip header lines
+            columns = line.split()
+            if len(columns) >= 2:
+                package_info = {
+                    "Package": columns[0],
+                    "Version": columns[1],
+                }
+                software_list.append(package_info)
 
-# # Print the list of installed software
-# for software in software_list:
-#     print(f"Package: {software['Package']}")
-#     print(f"Version: {software['Version']}")
-#     print()
+        return software_list
+    except subprocess.CalledProcessError:
+        return []
+
+def get_installed_software():
+    if platform.system() == "Linux":
+        return get_installed_software_linux()
+    elif platform.system() == "Windows":
+        return get_installed_software_windows()
+    else:
+        return []
