6067dab0702e1e9d348905e378920266fe645db4
Nguyen Van Khanh Nhan <nhanhoa21012002@gmail.com>
2023-08-26 20:22:12 +0700

    device commit

--- device_info.py
+++ device_info.py
@@ -1,5 +1,30 @@
-import win32com.client
 import platform
+
+# only import win32com.client if on Windows
+if platform.system() == "Windows":
+    import win32com.client
+elif platform.system() == "Linux":
+    from pyudev import Context
+
+def get_connected_mouse_devices_linux():
+    devices = []
+    context = Context()
+
+    list_devices = context.list_devices(subsystem="input")
+    # print(list_devices) to see what you can filter on
+    for device in list_devices:
+        device_name = device.get("NAME")
+        if device_name:
+            mouse_info = {
+                "name": device.get("NAME"),
+                "phys": device.get("PHYS"),
+                "id": device.get("ID"),
+                "properties": device.get("PROPERTIES"),
+                # Add more properties you're interested in...
+            }
+            print(mouse_info)
+
+    return devices
 
 
 def get_connected_mouse_devices_window():
@@ -22,10 +47,6 @@ def get_connected_mouse_devices_window():
             mouse_devices.append(mouse_info)
 
     return mouse_devices
-
-
-def get_connected_mouse_devices_linux():
-    pass
 
 
 def get_connected_mouse_devices():
