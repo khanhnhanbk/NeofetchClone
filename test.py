4887dc9dcdcf08cc9f0a72c7af08c9188f2b3100
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-10-11 15:43:03 +0700

    changeName

--- test.py
+++
@@ -1,4 +0,0 @@
-from windows_tools.installed_software import get_installed_software
-
-for software in get_installed_software():
-    print(software["name"], software["version"], software["publisher"])
